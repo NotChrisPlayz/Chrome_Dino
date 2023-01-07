264c8120c11d581ce4f6d9cb9ac27f1a72a5d5fc
Chris Playz Playz <121309792+NotChrisPlayz@users.noreply.github.com>
2023-01-07 17:31:48 +0000

    Added movement to cactus

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame, sys
 from pygame.locals import QUIT
 import random
+import time
 
 pygame.init()
 
@@ -12,30 +13,37 @@ score = "SCORE:", score
 # colors
 RED = (200, 0, 0)
 
-class Ground:
+class Cactus:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, l):
         self.x = x
         self.y = y
-        #self.ground = pygame.draw.rect(DISPLAY,"white",(0,250,600,500))
+        self.l = l
+        self.cactus = pygame.draw.rect(DISPLAY,"green",(150,220,30,30))
 
-ground = Ground(0,250)
+cactus1 = Cactus(150,220,30)
+cactus2 = Cactus(350,210,40)
+cactus3 = Cactus(550,200,50)
 
+cacti = [cactus1,cactus2,cactus3]
 
-#Ground.x = 0
-#Ground.y = 250
+clock = pygame.time.Clock()
 
-#def shiftGround():
- #   for road in roads:
-  #      road.y += 1
-   #     if road.y > 400:
-    #        road.y = -road.road.get_height()
+def shiftcactus():
+    for cactus in cacti:
+        cactus.x -= 1
+        
+        if cactus.x < 0:
+            cactus.x = 600
 
 while True:
+    clock.tick(60)
     DISPLAY.fill(RED)
-    pygame.draw.rect(DISPLAY,"white",(ground.x,ground.y,600,500))
+    pygame.draw.rect(DISPLAY,"white",(0,250,600,500))
     pygame.draw.rect(DISPLAY,"black",(30,185,30,65))
-    pygame.draw.rect(DISPLAY,"green",(150,220,30,30))
-    pygame.draw.rect(DISPLAY,"green",(350,210,30,40))
-    pygame.draw.rect(DISPLAY,"green",(550,200,30,50))
+    
+    pygame.draw.rect(DISPLAY,"green",(cactus1.x,cactus1.y,30,30))
+    pygame.draw.rect(DISPLAY,"green",(cactus2.x,cactus2.y,30,40))
+    pygame.draw.rect(DISPLAY,"green",(cactus3.x,cactus3.y,30,50))
+    shiftcactus()
     pygame.display.update()
