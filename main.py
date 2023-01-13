ff6d248dbd17a1d69920bc98802694a99cecd0f1
Chris Playz Playz <121309792+NotChrisPlayz@users.noreply.github.com>
2023-01-13 17:27:17 +0000

    added jumping motion to dino

--- main.py
+++ main.py
@@ -21,6 +21,24 @@ class Cactus:
         self.l = l
         self.cactus = pygame.draw.rect(DISPLAY,"green",(150,220,30,30))
 
+class Dino:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.jumping = False
+        self.coming_down = False
+        self.float = False
+        self.dino = pygame.draw.rect(DISPLAY,"black",(30,185,30,65))
+
+    def jump(self):
+        self.jumping = True
+
+    def come_down(self):
+        self.coming_down = True
+        #self.y += 10
+        
+dino = Dino(30,185)
+
 cactus1 = Cactus(150,220,30)
 cactus2 = Cactus(350,210,40)
 cactus3 = Cactus(550,200,50)
@@ -36,14 +54,44 @@ def shiftcactus():
         if cactus.x < 0:
             cactus.x = 600
 
+def controlDino():
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                dino.jump()
+
+        #if event.key == pygame.K_d:
+            #if car1.x < DISPLAY.get_width() - car1.car.get_width():
+                #car1.move_right()
+
+float = 0
+
 while True:
     clock.tick(60)
     DISPLAY.fill(RED)
     pygame.draw.rect(DISPLAY,"white",(0,250,600,500))
-    pygame.draw.rect(DISPLAY,"black",(30,185,30,65))
-    
-    pygame.draw.rect(DISPLAY,"green",(cactus1.x,cactus1.y,30,30))
-    pygame.draw.rect(DISPLAY,"green",(cactus2.x,cactus2.y,30,40))
-    pygame.draw.rect(DISPLAY,"green",(cactus3.x,cactus3.y,30,50))
+    pygame.draw.rect(DISPLAY,"black",(dino.x,dino.y,30,65))
+    for cactus in cacti:
+        pygame.draw.rect(DISPLAY,"green",(cactus.x,cactus.y,30,cactus.l))
     shiftcactus()
+    controlDino()
+    if dino.jumping:
+        dino.y-=1
+    if dino.y < 125:
+        dino.jumping = False
+        dino.float = True
+    if dino.float:
+        float += 1
+        if float > 50:
+            dino.float = False
+            dino.come_down()
+            float = 0
+    if dino.coming_down:
+        dino.y+=1
+    if dino.y >= 185:
+        dino.coming_down = False
     pygame.display.update()
