57bb62be176ad3e445fc86e90a4ce3fabccdb278
Chris Abhilash <121309792+NotChrisPlayz@users.noreply.github.com>
2023-01-27 21:55:47 +0400

    refactoring

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import pygame, sys
 from pygame.locals import QUIT
-import random
-import time
+
+pygame.init()
+DISPLAY = pygame.display.set_mode((600, 400))
+pygame.display.set_caption('Chrome Dino')
 
 game = "playing"
 
-pygame.init()
-
-dino_img = pygame.image.load("dino.png")
-dino_img = pygame.transform.scale(dino_img,(30,65))
+dino_img = pygame.transform.scale(pygame.image.load("dino.png"),(30,65))
 cactus_img = pygame.image.load("Cactus.png")
 cactus_img = pygame.transform.scale(cactus_img,(40,30))
 cactus_img2 = pygame.transform.scale(cactus_img,(40,40))
@@ -16,26 +15,20 @@ cactus_img3 = pygame.transform.scale(cactus_img,(40,45))
 
 cacti_img = [cactus_img,cactus_img2,cactus_img3]
 
-DISPLAY = pygame.display.set_mode((600, 400))
-pygame.display.set_caption('Chrome Dino')
-
 font = pygame.font.SysFont("Times New Roman",25)
 bigFont = pygame.font.SysFont("Times New Roman" , 40)
 
 LosingText = bigFont.render("GAME OVER",False, "black")
 
-
 # colors
 RED = (200, 0, 0)
 
-class Cactus:
-
+class Cactus: 
     def __init__(self, x, y, l):
         self.x = x
         self.y = y
         self.l = l
-        self.cactus = pygame.draw.rect(DISPLAY,"green",(150,220,30,30))
-
+        
 class Dino:
     def __init__(self, x, y):
         self.x = x
@@ -43,7 +36,7 @@ class Dino:
         self.jumping = False
         self.coming_down = False
         self.float = False
-        self.dino = pygame.draw.rect(DISPLAY,"black",(30,185,30,65))
+        self.float_energy = 0
         self.condition = "alive"
         self.score = 0
 
@@ -52,7 +45,6 @@ class Dino:
 
     def come_down(self):
         self.coming_down = True
-        #self.y += 10
         
 dino = Dino(30,185)
 
@@ -61,8 +53,6 @@ cactus2 = Cactus(350,210,40)
 cactus3 = Cactus(550,205,45)
 
 cacti = [cactus,cactus2,cactus3]
-
-clock = pygame.time.Clock()
 
 def shiftcactus():
     for cactus in cacti:
@@ -87,40 +77,40 @@ def collision():
         if abs(dino.x - cactus.x) <= 10:
             if dino.y + 85 > cactus.y:
                 dino.condition = "dead"
-    
 
-float = 0
-
-while True:
-    clock.tick(60)
-    DISPLAY.fill("white")
-    pygame.draw.rect(DISPLAY,"black",(0,250,600,10))
-    #pygame.draw.rect(DISPLAY,"black",(dino.x,dino.y,30,65))
-    DISPLAY.blit(dino_img,(dino.x,dino.y))
-    #DISPLAY.blit(cactus_img,(cactus.x,cactus.y))
-    for index,cactus in enumerate(cacti):
-        #pygame.draw.rect(DISPLAY,"green",(cactus.x,cactus.y,30,cactus.l))
-        DISPLAY.blit(cacti_img[index],(cactus.x,cactus.y))
-    shiftcactus()
-    controlDino()
+def handle_dino():
     if dino.jumping:
         dino.y-=1
     if dino.y < 120:
         dino.jumping = False
         dino.float = True
     if dino.float:
-        float += 1
-        if float > 50:
+        dino.float_energy += 1
+        if dino.float_energy > 50:
             dino.float = False
             dino.come_down()
-            float = 0
+            dino.float_energy = 0
     if dino.coming_down:
         dino.y+=1
     if dino.y >= 185:
         dino.coming_down = False
+    
+clock = pygame.time.Clock()
+
+while True:
+    clock.tick(60)
+    DISPLAY.fill("white")
+    pygame.draw.rect(DISPLAY,"black",(0,250,600,10))
+    DISPLAY.blit(dino_img,(dino.x,dino.y))
+    for index,cactus in enumerate(cacti):
+        DISPLAY.blit(cacti_img[index],(cactus.x,cactus.y))
+
+    shiftcactus()
+    controlDino()
+    handle_dino()
     collision()
     if dino.condition == "dead":
-        DISPLAY.fill("red")
+        DISPLAY.fill("white")
         ScoreText = font.render(f"Score:{dino.score}",False, "black")
         DISPLAY.blit(ScoreText,(10,65))
         DISPLAY.blit(LosingText,(10,20))
